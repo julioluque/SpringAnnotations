8ad161bbfda763485e9a420294ced401e5fe9a50
Julio Luque <julio.a.luque@gmail.com>
2020-05-03 01:15:27 -0300

    Solucion a sobrecarga de constructores y 4 opciones para @Autowired

--- src/ar/jluque/annotations/ComercialExperimentado.java
+++ src/ar/jluque/annotations/ComercialExperimentado.java
@@ -6,14 +6,36 @@ import org.springframework.stereotype.Component;
 @Component
 public class ComercialExperimentado implements Empleados {
 	
-	
-	private Informes informes;
+	/**
+	 * @Autowired -> 4 OPCIONES, pero solo lo puedo usar en un ...
+	 * 1 CAMPO DE CLASE -> Spring lo usa por medio de reflection
+	 * 2 CONSTRUCTOR -> Cuando hay sobre carga de constructores. Si hay solo uno, Spring lo invoca por defecto
+	 * 3 SETTER -> Opcional, pero muy usado cuando hay sobrecarga de constructores.
+	 * 4 METODO -> Misma funcionalidad que un setter, pero apto para cualquier tipo de metodo.
+	 */
 	
 	@Autowired
+	private Informes informes;
+	
+	public ComercialExperimentado() {
+	}
+	
+//	@Autowired
 	public ComercialExperimentado(Informes informes) {
 		this.informes = informes;
 	}
+	
+	
+//	@Autowired
+	public void setInformes(Informes informes) {
+		this.informes = informes;
+	}
 
+//	@Autowired
+	public void metodoNormal(Informes informes) {
+		this.informes = informes;
+	}
+	
 	@Override
 	public String getTareas() {
 		return "Comercial. Tarea: vender, vender y vender...";
